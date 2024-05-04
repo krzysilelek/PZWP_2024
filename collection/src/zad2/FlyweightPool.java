95b8be7044c176b41c109f998e32051c0d12ca93
krzysilelek <1kczarnecki@gmail.com>
2024-05-04 19:33:53 +0200

    Zadanie 2 - pierwsza próba (chyba dobrze).

---
+++ collection/src/zad2/FlyweightPool.java
@@ -0,0 +1,36 @@
+package zad2;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class FlyweightPool {
+	private volatile static FlyweightPool INSTANCE;
+	private Map<String, WeakReference<Item>> articlesPool;
+	
+	private FlyweightPool() {
+		articlesPool = new HashMap<String, WeakReference<Item>>();
+	}
+	
+	public static FlyweightPool getInstanceOf() {
+		if(INSTANCE==null)
+			synchronized(FlyweightPool.class) {
+			if(INSTANCE==null)
+			INSTANCE = new FlyweightPool();
+		}
+		return INSTANCE;
+	}
+	
+	public Item getArticle(String name, float price) {
+		
+		WeakReference<Item> ref = articlesPool.get(name);
+        Item article = (ref != null) ? ref.get() : null;
+
+        if (article == null) {
+            article = new Item(name, price);
+            articlesPool.put(name, new WeakReference<Item>(article));
+        }
+
+        return article;
+	}
+}
--- collection/src/zad2/Test.java
+++ collection/src/zad2/Test.java
@@ -3,8 +3,18 @@ package zad2;
 public class Test {
 
     public static void main(String[] args) {
-
-        Warehouse[] warehouses = new Warehouse[100];
+    	
+        experiment0();
+        experiment1();
+        
+    }
+    
+    private static void experiment0() {
+    	Runtime runtime = Runtime.getRuntime();
+    	runtime.gc();
+        long initialMemory = runtime.totalMemory() - runtime.freeMemory();
+        
+    	Warehouse[] warehouses = new Warehouse[100];
         for (int i = 0; i < warehouses.length; i++) {
             warehouses[i] = new Warehouse();
             for (int j = 0; j < 20; j++) {
@@ -12,10 +22,52 @@ public class Test {
             }
         }
 
+        /*
         for (Warehouse m : warehouses) {
             for (Item a : m.getItems()) {
                 System.out.println(a.getName() + " " + a.getPrice());
             }
         }
+        */
+        
+        runtime.gc();
+        long finalMemory = runtime.totalMemory() - runtime.freeMemory();
+        long memoryUsed = finalMemory - initialMemory;
+
+        System.out.println("Normal usage: " + memoryUsed + " bytes");
+    }
+    
+    private static void experiment1() {
+    	Runtime runtime = Runtime.getRuntime();
+    	runtime.gc();
+        long initialMemory = runtime.totalMemory() - runtime.freeMemory();
+        
+        FlyweightPool pool = FlyweightPool.getInstanceOf();
+        
+    	Warehouse[] warehouses = new Warehouse[100];
+        for (int i = 0; i < warehouses.length; i++) {
+            warehouses[i] = new Warehouse();
+            for (int j = 0; j < 20; j++) {
+                warehouses[i].addItem(pool.getArticle("art" + j, j));
+            }
+        }
+        
+        /*
+        for (Warehouse m : warehouses) {
+        	Item a = m.getItems().get(0);
+        	Item b = m.getItems().get(1);
+        	System.out.println(a+" "+m);
+        	System.out.println(b+" "+m);
+        	for (Item a : m.getItems()) {
+                System.out.println(a+" "+m);
+            }
+        }
+        */
+        
+        runtime.gc();
+        long finalMemory = runtime.totalMemory() - runtime.freeMemory();
+        long memoryUsed = finalMemory - initialMemory;
+
+        System.out.println("Flyweight usage: " + memoryUsed + " bytes");
     }
 }
